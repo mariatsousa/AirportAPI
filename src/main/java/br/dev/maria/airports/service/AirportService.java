43961efa7993b75b5db12c84d9b7990905e89da3
mariatsousa <mariaduda311207@gmail.com>
2025-02-28 10:24:06 -0300

    Termino fase 5 Update interface AirportRepository - Implementado assinatura de método findByIataCode(); Update AirportService - iplementado metodo findByIataCodeSearch. Update AirportController - Adicionado endpoint /iataCode{iataCode}

--- src/main/java/br/dev/maria/airports/service/AirportService.java
+++ src/main/java/br/dev/maria/airports/service/AirportService.java
@@ -43,4 +43,9 @@ public class AirportService {
                 .map(x -> new AirportMinDTO(x)).toList();
         return resultDTO;
     }
+    
+    public Airport findByIataCode (String iataCode) {
+        Airport result = airportRepository.findByIataCode (iataCode);
+        return result;
+    }
 }
